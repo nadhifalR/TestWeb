9840c718b4055bfea1ba0d55e407f6d91725e87b
nadhifalR <nadhifal01@gmail.com>
2026-02-13 16:00:21 +0700

    build: remove explicit chunking for React dependencies from Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,7 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-              return 'react-vendor';
-            }
+
             if (id.includes('@nivo')) {
               return 'nivo';
             }
